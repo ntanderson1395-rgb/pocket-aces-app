155b049085b5c638c78f3ab262c2ff1e1c4c37f0
Nate <ntanderson@Nates-MacBook-Pro.local>
2026-04-19 20:07:03 -0500

    Updates

--- _next/static/bexrsc3CrWByqBCjODTE5/_buildManifest.js
+++ _next/static/bexrsc3CrWByqBCjODTE5/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/pocket-aces-app//_next/:path+",
-        "destination": "/pocket-aces-app/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
